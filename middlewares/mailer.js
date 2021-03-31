c719bcd173a284209e4f3488123a8ea4817603a1
talk2mat2 <62118678+talk2mat2@users.noreply.github.com>
2021-03-31 22:44:04 +0100

    updated functionalities and corrected some bugs as client request

--- middlewares/mailer.js
+++ middlewares/mailer.js
@@ -88,8 +88,8 @@ exports.sendmail= async function (email,confirmationCode,res) {
     <p>Thank you for your interest in <b>IFF- (Intergrity Family And Friends)</b> . Please confirm your email by clicking on the following link
     </p> <a href=${process.env.proxyUrl}/verifyEmail/?token=${confirmationCode}&email=${email}> click here </a>
     <p>or by clicking on this button </p><a href=${process.env.proxyUrl}/verifyEmail/?token=${confirmationCode}&email=${email}> <button  style="color:white;background-color:tomato;">Click here</button><a/>
-   you can also follow this linl if the button is didn't work
-   ${process.env.proxyUrl}/verifyEmail/?token=${confirmationCode}&email=${email}
+   you can also follow this link if the button is didn't work <br /><br/>
+<pre> ${process.env.proxyUrl}/verifyEmail/?token=${confirmationCode}&email=${email}</pre>
     <p> Cheers,</>
 <p>Iff - Intergrity Family And Friends</>
     </div>`,
